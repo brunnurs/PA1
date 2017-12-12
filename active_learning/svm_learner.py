69a62242f2940ccd606e85a3f3d8647ba0d7bed4
Brunner Ursin <ursin.brunner@ti8m.ch>
2017-12-12 13:11:55 +0100

    use the same svm learner as in the passive mode. Print svm learner at a certain amount of training examples

--- active_learning/svm_learner.py
+++ active_learning/svm_learner.py
@@ -1,21 +1,51 @@
-from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+import numpy as np
 
+from sklearn.svm import SVC
 from active_learning.learner import Learner
 
 
 class SvmLearner(Learner):
     def __init__(self):
         print('Initialize an SVM learner')
-        self.svm_classifier = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-            decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
-            max_iter=-1, probability=False, random_state=None, shrinking=True,
-            tol=0.001, verbose=False)
+        self.clf = SVC(C=10, gamma=10, kernel='rbf', class_weight=None, probability=True)
+        # self.clf = SVC(C=100, kernel='linear', class_weight=None, probability=True)
 
         self.prediction = []
 
     def fit(self, x, y):
-        self.svm_classifier.fit(x, y)
+        self.clf.fit(x, y)
+
+        # if len(x) == 20 or len(x) == 60 or len(x) == 300 or len(x) == 600 or len(x) == 990:
+        #     self.plot_classifier_and_scatter_data(self.clf, x, y)
 
     def predict(self, x):
-        self.prediction = self.svm_classifier.predict(x)
+        self.prediction = self.clf.predict(x)
         return self.prediction
+
+    @staticmethod
+    def plot_classifier_and_scatter_data(clf, x, y):
+        # plot data points
+        plt.scatter(x[:, 0], x[:, 1], c=y, s=30)
+
+        # plot the decision function
+        ax = plt.gca()
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        # create grid to evaluate model
+        xx = np.linspace(xlim[0], xlim[1], 30)
+        yy = np.linspace(ylim[0], ylim[1], 30)
+        YY, XX = np.meshgrid(yy, xx)
+        xy = np.vstack([XX.ravel(), YY.ravel()]).T
+        Z = clf.decision_function(xy).reshape(XX.shape)
+        # plot decision boundary and margins
+        ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1], alpha=0.5,
+                   linestyles=['--', '-', '--'])
+        # plot support vectors
+        ax.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=100,
+                   linewidth=1, facecolors='none')
+
+        plt.ylabel('feature #1')
+        plt.xlabel('feature #2')
+
+        plt.show()
