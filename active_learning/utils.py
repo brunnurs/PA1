b88a84d7458cba20ea0de856b68afdeb69b47ffc
Brunner Ursin <ursin.brunner@ti8m.ch>
2017-12-12 13:13:04 +0100

    new method to get stratified random indices

--- active_learning/utils.py
+++ active_learning/utils.py
@@ -1,5 +1,23 @@
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+
+def stratified_random_indices(data, number_of_random_indices):
+    """
+    Return a certain amount of indices randomly. THe data array is used for the range.
+    Important: the result is stratified, so it contains the same percentage of a certain class as all the other indices.
+    :param data:
+    :param number_of_random_indices:
+    :return:
+    """
+    x = np.array(transform_to_feature_vector(data))
+    y = np.array(list(map(lambda r: int(r['ground_truth']), data)))
+
+    indices = range(len(x))
+
+    _, _, _, _, indices_train, _ = train_test_split(x, y, indices, train_size=number_of_random_indices, stratify=y)
+
+    return indices_train
 
 
 def transform_to_labeled_feature_vector(labeled_data):
